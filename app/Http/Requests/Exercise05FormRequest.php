516eb684b90c6176c4cdd78e4227adc8f12408ee
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-03-08 00:45:35 +0300

    complete exercise #5

--- app/Http/Requests/Exercise05FormRequest.php
+++ app/Http/Requests/Exercise05FormRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use App\Models\SkillPosting;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 /**
@@ -20,6 +21,51 @@ class Exercise05FormRequest extends FormRequest
     // You can find a list of valid categories in /app/Models/SkillPosting.php
     public function rules(): array
     {
-        return [];
+        //     Functional requirements
+        //     * One of the categories should be designated as the primary category
+        //     * A skill can be offered onsite, remote, or both
+
+        return [
+            'title' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'categories' => [
+                'required',
+                'array',
+                'min:1',
+                Rule::in(SkillPosting::VALID_CATEGORIES),
+            ],
+            'categories.*' => [
+                'string',
+                'distinct',
+            ],
+            'primary_category' => [
+                'required',
+                'string',
+                'in_array:categories',
+            ],
+            'is_remote' => [
+                'required_without:is_onsite',
+                'boolean'
+            ],
+            'is_onsite' => [
+                'required_without:is_remote',
+                'boolean'
+            ],
+            'minimum_rate' => [
+                'required',
+                'integer',
+                'between:0,65535',
+            ],
+            'maximum_rate' => [
+                'required',
+                'integer',
+                'between:0,65535',
+                'gte:minimum_rate',
+            ],
+
+        ];
     }
 }
