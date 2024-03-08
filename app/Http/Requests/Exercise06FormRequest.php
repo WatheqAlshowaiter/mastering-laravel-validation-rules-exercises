88b956796dfdc8157e8847d7174780eb0efe209e
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-03-08 04:07:40 +0300

    complete exercise #6

--- app/Http/Requests/Exercise06FormRequest.php
+++ app/Http/Requests/Exercise06FormRequest.php
@@ -17,6 +17,37 @@ class Exercise06FormRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'workflow_type' => [
+                'required',
+                'string',
+                'in:shipping,receiving',
+            ],
+            'processed_date' => [
+                'required',
+                'string',
+                'date_format:Y-m-d',
+                'before_or_equal:today',
+            ],
+            'waybill_number' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'is_damaged' => [
+                'prohibited_unless:workflow_type,receiving',
+                'boolean',
+            ],
+            'damage_description' => [
+                'required_if:is_damaged,true',
+                'string',
+                'max:1500',
+            ],
+            'notes' => [
+                'nullable',
+                'string',
+                'max:1500',
+            ],
+        ];
     }
 }
