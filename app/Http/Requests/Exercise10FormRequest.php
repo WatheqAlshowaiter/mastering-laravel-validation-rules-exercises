765502aae57f5f9ea31023718836a0e923ece1e3
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-03-08 04:57:29 +0300

    complete exercise #10

--- app/Http/Requests/Exercise10FormRequest.php
+++ app/Http/Requests/Exercise10FormRequest.php
@@ -17,6 +17,35 @@ class Exercise10FormRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+
+            'schedule_type'=>[
+                'required',
+                'string',
+                'max:255',
+                'in:date,days'
+            ],
+            'date'=>[
+                'required_if:schedule_type,date',
+                'string',
+                'max:255',
+                'date_format:Y-m-d',
+                'after_or_equal:today'
+            ],
+            'days'=>[
+                'required_if:schedule_type,days',
+                'array',
+            ],
+            'days.*'=>[
+                'integer',
+                'distinct',
+                'between:1,7'
+            ]
+        ];
     }
 }
